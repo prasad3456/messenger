d02744f6e59403cb95539d9c5056f9d6514fa646
OM <OM@LAPTOP-LRQAP85V>
2017-02-06 15:10:16 -0600

    I created service and model

---
+++ src/main/java/org/prasad/webservcies/messenger/service/MessageService.java
@@ -0,0 +1,17 @@
+package org.prasad.webservcies.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.prasad.webservcies.messenger.model.Message;
+
+public class MessageService {
+
+	public List<Message> getAllMessages(){
+		Message m1=new Message(1L, "Hello World", "Prasad");
+		Message m2=new Message(2L, "Hello Jersey", "Venkata");
+		List<Message> list=new ArrayList<>();
+		list.add(m1);
+		list.add(m2);
+		return list;
+	}
+}
